03a719e4a009587081b8fcf986e533b087f4898d
jayrich <jay@jaymade.com>
2018-12-05 16:32:12 -0500

    input added

--- src/app/images/image/image.component.ts
+++ src/app/images/image/image.component.ts
@@ -9,20 +9,43 @@ import { Image } from '../../models/image.model';
 export class ImageComponent implements OnInit {
   imgOrientation = 'landscape';
   title = 'photograph';
-  photo: Image = {
+  startOrient = 'landscape';
+  selectedOrient = '';
+
+  orients: any = ['landscape', 'portrait'];
+
+  landscape: Image = {
     id: '8a54df69fto',
-    src: 'assets/portrait.jpg',
-    addedDate: '11/21/2018',
-    name: 'landscape',
+    src: 'assets/landscape.jpg',
+    addedDate: '10/25/2018',
+    name: 'Landscape',
     description: 'a family picture',
     project: 'family',
     splash: true
   };
+  portrait: Image = {
+    id: '8a54df69fto',
+    src: 'assets/portrait.jpg',
+    addedDate: '10/25/2018',
+    name: 'Portrait',
+    description: 'a family picture',
+    project: 'family',
+    splash: true
+  };
+  photo = {};
 
   constructor() {}
 
   ngOnInit() {}
 
+  radioChangeHandler(event: any) {
+    this.selectedOrient = event.target.value;
+    if (event.target.value === 'portrait') {
+      this.photo = this.portrait;
+    } else {
+      this.photo = this.landscape;
+    }
+  }
   // checkPicOrentation() {
   //   if (this.photo.width > this.photo.height) {
   //     imgOrientation = 'landscape';
